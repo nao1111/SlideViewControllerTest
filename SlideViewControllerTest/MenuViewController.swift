30c5ac8f0eb6fc342d42d5c243c7567c2c421ff1
nao <nao@cincs.net>
2015-09-10 11:53:57 +0900

    表示用のControllerを追加・設定

---
+++ SlideViewControllerTest/MenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.swift
+//  SlideViewControllerTest
+//
+//  Created by nao on 2015/09/10.
+//  Copyright (c) 2015年 nao. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UITableViewController {
+    
+}
---
+++ SlideViewControllerTest/NavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  NavigationController.swift
+//  SlideViewControllerTest
+//
+//  Created by nao on 2015/09/10.
+//  Copyright (c) 2015年 nao. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+    
+}
